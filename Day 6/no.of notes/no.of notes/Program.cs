dd31c831055548358cf706af79b093a8aa2794f9
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-04-15 15:16:10 +0530

    Add files via upload

---
+++ Day 6/no.of notes/no.of notes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace no.of_notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount: ");
+            int amt=int.Parse(Console.ReadLine());
+
+            int[] notes = { 2000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+            
+
+            
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    int n = amt / notes[i];
+                    amt=amt% notes[i];
+                    Console.WriteLine(n);
+                }
+            }
+        }
+    }
+}
