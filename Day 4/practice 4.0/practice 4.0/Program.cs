cf9cd675bdaaaf68fd97efb20349c516066906a5
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-04-15 09:51:22 +0530

    Add files via upload

---
+++ Day 4/practice 4.0/practice 4.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_4._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter the string: ");
+            str= Console.ReadLine();
+            Console.WriteLine("The string you enetered is {0}",str);
+          
+        }
+    }
+}
---
+++ Day 4/practice 4.1/practice 4.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i;
+            Console.WriteLine("Input 10 elements in the array :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\nElements in array are: ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0}  ", arr[i]);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
