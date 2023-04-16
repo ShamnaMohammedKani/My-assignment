5fb5917cd610d3a3a931654bf5cd4df1b14d5cc9
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-04-16 21:34:33 +0530

    Add files via upload

---
+++ Telephone/Telephone/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ElectronicPhone a = new ElectronicPhone("Digital");
+            a.Ring();
+        }
+    }
+    class Telephone
+    {
+        protected string PhoneType;
+        public string PHoneType
+        {
+            get { return PhoneType; }
+            set { PhoneType = value; }
+        }
+
+
+        public Telephone()
+        {
+
+        }
+        public Telephone(string phoneType)
+        {
+            PhoneType = phoneType;
+        }
+
+
+        public void Ring()
+        {
+            Console.WriteLine("Ringing the" + PhoneType);
+        }
+
+    }
+    class ElectronicPhone : Telephone
+    {
+        public ElectronicPhone()
+        {
+
+        }
+        public ElectronicPhone(string phoneType) : base(phoneType)
+        {
+
+        }
+    }
+}
+
+   
+
---
+++ orange fruit/orange fruit/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orange_fruit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Orange o1 = new Orange(1, "ORANGE", "MAHARASHTRA");
+            o1.display();
+
+
+        }
+
+
+    }
+
+    public class Fruit
+    {
+        int number;
+        string name;
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                if (number != 0) number = value;
+                else
+                    Console.WriteLine("Invalid");
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != null) name = value;
+                else
+                    Console.WriteLine("Invalid");
+            }
+        }
+
+        public Fruit()
+        {
+
+        }
+        public Fruit(int Number, string Name)
+        {
+            number = Number;
+            name = Name;
+
+
+        }
+        public void display()
+        {
+            Console.WriteLine(number + "\n" + name);
+        }
+
+    }
+    public class Orange : Fruit
+    {
+
+        string[] place = { "MAHARASHTRA", "TELUNGANA", "KARNATAKA" };
+        string _origin;
+        public string origin
+        {
+            get { return _origin; }
+            set
+            {
+                if (place.Contains(value))
+                { _origin = value; }
+            }
+        }
+        public Orange(int num, string name, string abc) : base(num, name)
+        {
+
+            this._origin = abc;
+        }
+        public new void display()
+        {
+            Console.WriteLine(_origin);
+            base.display();
+        }
+    }
+
+}
+
+
