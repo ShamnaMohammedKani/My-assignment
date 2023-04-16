84da19cf4069517dd8ed6b3bcbc5c2718b091d04
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-04-16 21:45:09 +0530

    Add files via upload

---
+++ Day 10/abstract/abstract/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure f = new Square();
+            f.Dimension = 10;
+
+        }
+
+        abstract class Figure
+        {
+            public int Dimension;
+            public abstract double Area();
+            public abstract double Perimeter();
+
+        }
+        class Square : Figure
+        {
+            public override double Area()
+            {
+                return Dimension * Dimension;
+
+            }
+            public override double Perimeter()
+            {
+                return 4 * Dimension;
+            }
+        }
+        class Circle : Figure
+        {
+            public override double Area()
+            {
+                return Math.PI * Dimension * Dimension;
+            }
+            public override double Perimeter()
+            {
+                return Math.PI * 2 * Dimension;
+            }
+        }
+
+    }
+}
+
