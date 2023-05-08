212b70b666f0fe7efc135c1b3df501adc8fc185d
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-05-08 10:49:47 +0530

    Add files via upload

---
+++ Day24/student/student/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    public class student
+    {
+        static void Main(string[] args)
+        {
+             public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+    
+    }
+}
+
