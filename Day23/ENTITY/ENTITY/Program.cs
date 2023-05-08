77fe443215b43530b1c91879c455637ab3bb826a
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-05-08 09:44:50 +0530

    Add files via upload

---
+++ Day23/ENTITY/ENTITY/Program.cs
@@ -0,0 +1,71 @@
+﻿using ENTITY;
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace EFDemo1
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            ENTEntities e1 = new ENTEntities();
+
+            foreach (var item in e1.Dpts)
+
+            {
+
+                Console.WriteLine(item.DeptName);
+
+            }
+
+            /*  using (var context = new ENTEntities())
+
+              {
+
+                  var dept = new Dpt()
+
+                  {
+
+                      DeptID = 110,
+
+                      DeptName = "IT"
+
+                  };
+
+                  context.Dpts.Add(dept);
+
+                  context.SaveChanges();
+
+              }*/
+            using (var context = new ENTEntities())
+
+            {
+
+                var dept = context.Dpts.First<Dpt>();
+
+                dept.DeptName = "kulam";
+
+                context.SaveChanges();
+
+            }
+
+
+
+        }
+
+    }
+
+}
---
+++ Day23/ExtensionMethod2/ExtensionMethod2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod2
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "india is great";
+            string result = s.mupper();
+            Console.WriteLine(result);
+        }
+        public static string mupper(this string s)
+        {
+            return s.ToUpper();
+
+        }
+    }
+}
