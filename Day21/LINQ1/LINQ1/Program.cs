9e40a173ab189cc60a4e7a82d383ff0d69eef2f2
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-05-08 09:38:51 +0530

    Add files via upload

---
+++ Day21/LINQ1/LINQ1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] flowers = { "dahlia", "rose", "lotus", "lilly", "hibiscus", "dafodil" };
+
+            var fQuery =
+
+              from flower in flowers
+
+              where (flower.StartsWith("d"))
+
+              select flower;
+
+            foreach (string f in fQuery)
+
+            {
+
+                Console.WriteLine(f);
+
+            }
+
+        }
+    }
+}
