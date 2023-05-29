6ff4b144feb582340e3e469a97f3f75901450694
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-05-29 09:32:04 +0530

    Add files via upload

---
+++ Day42/1Darray/1Darray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Darray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter num of elements");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter elements");
+
+            int[] ar = new int[n];
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                ar[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            foreach (int i in ar)
+
+            {
+
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
