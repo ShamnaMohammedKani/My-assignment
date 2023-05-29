27bdc166ad5394a6adfc39403dcae39d9060ed21
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-05-29 09:28:47 +0530

    Add files via upload

---
+++ Day39/PowerOftwo/PowerOftwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOftwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = isPowerOfTwo(8);
+
+            Console.WriteLine(check);
+
+            Console.ReadKey();
+
+        }
+
+        public static bool isPowerOfTwo(int x)
+
+        {
+
+            while (((x % 2) == 0) && x > 1)
+
+            {
+
+                x /= 2;
+
+            }
+
+            return (x == 1);
+
+        }
+    }
+    }
+
