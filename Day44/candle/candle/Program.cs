67d249c74c8fa87aad9c5148b4d0623411adf743
ShamnaMohammedKani <129932055+ShamnaMohammedKani@users.noreply.github.com>
2023-05-29 09:34:13 +0530

    Add files via upload

---
+++ Day44/candle/candle/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+class Result
+
+{
+
+    public static int Candles(List<int> candles)
+
+    {
+
+        int maxHeight = 0;
+
+        int count = 0;
+
+        foreach (int height in candles)
+
+        {
+
+            if (height > maxHeight)
+
+            {
+
+                maxHeight = height;
+
+                count = 1;
+
+            }
+
+            else if (height == maxHeight)
+
+            {
+
+                count++;
+
+            }
+
+        }
+
+        return count;
+
+    }
+
+}
+
+class Solution
+
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.Candles(candles);
+
+        Console.WriteLine(result);
+
+    }
+
+}
+
+
